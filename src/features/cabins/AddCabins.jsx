3aede385c7cc9786c7d0b3c7dc60e80a70c54e6d
Yadier Martinez <yadieralexander19@gmail.com>
2023-07-02 18:44:23 -0400

    Update readme

--- src/features/cabins/AddCabins.jsx
+++ src/features/cabins/AddCabins.jsx
@@ -1,7 +1,6 @@
 import CreateCabinForm from "./CreateCabinForm";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
-import CabinTable from "./CabinTable";
 
 function AddCabins() {
   return (
